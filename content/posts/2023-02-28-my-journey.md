77f175a110be4d2575fa19fc56d2301f72f26ec0
kevinradp <kevinradp@gmail.com>
2023-02-28 15:50:12 +0400

    Add files

---
+++ content/posts/2023-02-28-my-journey.md
@@ -0,0 +1,36 @@
+---
+title: "Yoga Nidra: A Journey into the Subconscious"
+slug:
+description: "Yoga nidra experience"
+date: 2023-02-28 11:31:10
+author: kevin-rad
+tags: 
+    - personal-stories
+cover:
+fullscreen: false
+---
+
+Yoga Nidra, also referred to as "yogic sleep," is an incredibly potent meditation technique that has the ability to take you on a profound journey deep into your subconscious mind. Ever since discovering it through Andrew Huberman's podcasts, I have been regularly practicing yoga nidra for a while now. During a recent session, I had an experience that left me feeling both inexplicably rested and somewhat surreal.
+
+> "It was as if my body was no longer bound to the earth and I was
+> floating freely in space."
+
+I settled into my practice, listening to the guide's soothing voice as I relaxed into a state of deep relaxation. As the minutes passed, I began to feel a sensation of levitation throughout my body. It was as if my body was no longer bound to the earth and I was floating freely in space.
+
+> "...as if I was being vacuumed into a portal, a black oval that
+> grew larger and larger as I let myself go."
+
+As I went deeper into my sensations, I suddenly found myself in a new experience. It was as if I was being vacuumed into a portal, a black oval that grew larger and larger as I let myself go. I felt myself fighting against it, afraid to go further into the unknown.
+
+But as I fought back, I began to realize that this experience was not to be feared. It was a journey into my subconscious mind, a place where I could explore my deepest thoughts, emotions, and desires. I felt a sense of wonder and curiosity as I allowed myself to surrender to the experience.
+
+When the practice ended, I felt a deep sense of rest and relaxation, as if I had just awoken from a deep and restful sleep. The experience was surreal, yet transformative. It allowed me to tap into a deeper level of awareness and connect with a part of myself that I don't often access in my daily life.
+
+If you're feeling stressed, anxious, or just plain curious, you should give Yoga Nidra a try. It's a powerful way to explore your inner world and tap into your creativity. And who knows, you might just end up floating in space too (figuratively, of course). 
+
+>"Yoga is not about touching your toes, 
+>it's about what you learn on the way down."
+>~Jigar
+
+
+
---
+++ src/pages/About.vue
@@ -0,0 +1,91 @@
+<template>
+  <Layout>
+    <main>
+      <header class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto text-center px-6 pt-24">
+        <h1 class="text-4xl sm:text-5xl md:text-6xl font-sans font-bold mb-1">About me</h1>
+        <p class="text-grey-dark text-lg sm:text-3xl">&hellip;</p>
+      </header>
+      <nav class="absolute top-0 left-0 z-20 mt-6 ml-6">
+        <g-link to="/" class="text-gray-900 border-gray-400 text-sm border opacity-75 hover:opacity-100 rounded-full px-4 py-2 transition-opacity duration-300">&larr; Home</g-link>
+      </nav>
+      <article class="max-w-xl md:max-w-2xl xl:max-w-3xl mx-auto px-6 sm:px-12 pt-16 border-b border-gray-300 pb-10 mb-16">
+
+        <div class="markdown text-lg leading-normal text-gray-700">
+
+          <h3>Well, hello there!</h3>
+
+          <p>My name is Kevin Rad, and I'm the lucky turtle behind this blog. I'm so thrilled to have you here to join me on my wild and wacky journey through life, technology, and everything in between.</p>
+          
+          <p>I've always been a bit of a tech nerd, and I love coding (when it worked haha! but mostly furious when there's too many bugs), exploring new gadgets and gizmos. I mean, who doesn't love the thrill of 
+          learning a new tech and praying that you don't accidentally hit the self-destruct button? But beyond the tech stuff, I'm also all about 
+          living life to the fullest and having fun along the way.</p>
+
+          <p>I've learned a lot over the years - sometimes the hard way - and I love sharing my experiences with others. Whether it's a 
+          hilarious story about a misadventure with a new buzzwords or a deep realization about the meaning of life, I always try to keep it 
+          real and share what I've learned.</p>
+
+          <p>My hope is that through my blog, I can help others navigate the ups and downs of life and tech with a little more humor and grace. 
+          We're all in this together, after all!</p>
+          
+          <p>And as we navigate this crazy journey, I'll leave you with this quote from the tech legend, Steve Jobs: </p>
+          
+          <blockquote>
+            <p>"The only way to do great work is to love what you do. If you haven't found it yet, keep looking. 
+            Don't settle. As with all matters of the heart, you'll know when you find it."</p>
+          </blockquote>
+
+          <p>Keep on searching, friends, and never settle for anything less than what sets your heart on fire.</p>
+
+        </div>
+      </article>
+      <site-footer class="pb-8 sm:pb-10" />
+    </main>
+  </Layout>
+</template>
+
+<script>
+import config from '~/.temp/config.js'
+import SiteFooter from '@/components/Footer'
+
+export default {
+  components: {
+    SiteFooter
+  },
+  metaInfo () {
+    return {
+      title: 'About',
+      meta: [
+        {
+          key: 'description',
+          name: 'description',
+          content: this.ogDescription
+        },
+
+        { property: "og:type", content: 'article' },
+        { property: "og:title", content:'About' },
+        { property: "og:description", content: this.ogDescription },
+        { property: "og:url", content: `${this.config.siteUrl}/about/` },
+        { property: "og:image", content: this.ogImageUrl },
+
+        { name: "twitter:card", content: "summary_large_image" },
+        { name: "twitter:title", content: 'About' },
+        { name: "twitter:description", content: this.ogDescription },
+        { name: "twitter:site", content: "@cossssmin" },
+        { name: "twitter:creator", content: "@cossssmin" },
+        { name: "twitter:image", content: this.ogImageUrl },
+      ],
+    }
+  },
+  computed: {
+    config () {
+      return config
+    },
+    ogDescription () {
+      return 'Introduction to the Bleda blog starter for Gridsome.'
+    },
+    ogImageUrl () {
+      return `${this.config.siteUrl}/images/bleda-card.png`
+    }
+  },
+}
+</script>
